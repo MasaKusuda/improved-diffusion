a218e5d2c8a583447ca10491085ab0065dcb9194
MasaKusuda <masa.kusuda.1999@gmail.com>
2021-09-09 15:55:01 +0000

    changed:env.sh

--- experiments/learn_10Ksteps/env.sh
+++ experiments/learn_10Ksteps/env.sh
@@ -4,6 +4,3 @@ MODEL_FLAGS="--image_size 64 --num_channels 128 --num_res_blocks 3"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 64 --lr_anneal_steps 10000"
 ENV_PATH="--env_path "$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-
-# For sampling
-SAMPLING_FLAGS='--num_samples 100 --model_path ""'
